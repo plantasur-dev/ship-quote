c16cd92de24082d19ac91741d9418a64e2660816
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-08 09:17:33 +0200

    feat: add error-handling middleware

--- api/app.js
+++ api/app.js
@@ -7,6 +7,8 @@ import "./config/db.config.js";
 
 import router from "./config/routes.config.js";
 
+import { errorHandler } from './middlewares/errors.middleware.js';
+
 const app = express()
 
 const PORT = process.env.PORT || 3000;
@@ -16,6 +18,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use('/api/v1', router);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)

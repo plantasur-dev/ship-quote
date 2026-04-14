6752f7db6a8e2935333c2b0579f54141de75f316
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-14 15:46:19 +0200

    refactor: move middleware to new structure | feat: add middleware to ensure POST, PATCH and PUT requests include a body

---
+++ api/src/api/middlewares/errors.middleware.js
@@ -0,0 +1,32 @@
+
+import createHttpError from "http-errors";
+
+export const errorHandler = (err, req, res, next) => {
+
+    if (err.name === 'ValidationError') {
+        res.status(400).json(err.errors)
+        return;
+    }
+
+    if (err.status) {
+        res.status(err.status).json({ message: err.message });
+        return;
+    }
+
+    if (err.name === 'CastError') {
+        res.status(404).json({ message: 'Resource not found' });
+        return;
+    }
+
+    if (err.message?.includes('E11000')) {
+        res.status(409).json({ message: 'Resource duplicate' });
+        return;
+    }
+
+    console.error('Error internal server', err.message);
+    res.status(500).json({ message: 'Error internal server' });
+};
+
+export const routerNotFound = (req, res) => {
+    throw new createHttpError(404, "Route Not Found");
+};

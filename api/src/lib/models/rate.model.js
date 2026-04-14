7a178d522081f42bd82f26a36ea0f6eefb003192
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-14 15:41:52 +0200

    refactor: add lib folder for configuration, models, and stores

---
+++ api/src/lib/models/rate.model.js
@@ -0,0 +1,73 @@
+
+import mongoose from "mongoose";
+
+const servicePriceSchema = new mongoose.Schema({
+    service: {
+        type: String,
+        enum: ['economy', 'premium', 'express', 'basic'],
+        default: 'basic'
+    },
+    priceBreaks: [{
+        min: Number,
+        max: Number,
+        price: Number
+    }]
+}, { 
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+        virtuals: true,
+        transform: function (doc, ret) {
+            delete ret._id;
+        },
+    }
+});
+
+const rateSchema = new mongoose.Schema({
+    agencyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Agency",
+        index: true
+    },
+    type: {
+        type: String,
+        enum: ["pallet", "parcel"],
+        required: true
+    },
+    zoneName: { 
+        type: String, 
+        required: true 
+    },
+    palletTypeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "PalletType",
+        default: null
+    },
+    services: {
+        type: [servicePriceSchema]
+    },
+    calculationType: {
+        type: String,
+        enum: ["unit", "quantity"],
+        default: "unit"
+    }
+}, { 
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+        virtuals: true,
+        transform: function (doc, ret) {
+            delete ret._id;
+        },
+    }
+});
+
+rateSchema.index({ 
+    agencyId: 1, 
+    type: 1, 
+    zoneName: 1 
+});
+
+const Rate = mongoose.model("Rate", rateSchema);
+
+export default Rate;

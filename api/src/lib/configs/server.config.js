3c3fd2adbfd2c576f905f01681d087bbcce9b37e
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-14 15:41:52 +0200

    refactor: add lib folder for configuration, models, and stores

---
+++ api/src/lib/configs/server.config.js
@@ -0,0 +1,10 @@
+
+import { connectDB } from "./db.config.js";
+import { initLocations } from "../../api/services/zoneLocation.services.js";
+
+async function startServer() {
+    await connectDB();
+    await initLocations();
+}
+
+startServer();

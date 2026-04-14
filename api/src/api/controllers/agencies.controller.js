734faa9fd5d102a5c471ab9ff8d1221b89f1deef
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-14 15:51:01 +0200

    feat: add controllers api

---
+++ api/src/api/controllers/agencies.controller.js
@@ -0,0 +1,40 @@
+
+import createHttpError from "http-errors";
+
+import Agency from "../../lib/models/agency.model.js";
+
+export async function create(req, res) {
+
+    const { name, code, type, rules } = req.body;
+
+    const agency = await Agency.create({ 
+        name, 
+        code: code.toLowerCase(), 
+        type, 
+        rules 
+    });
+
+    res.status(201).json(agency);
+};
+
+export async function list(req, res) {
+    
+    const agencies = await Agency.find();
+
+    if (!agencies) throw createHttpError(404, 'Agencies not found');
+
+    res.json(agencies);
+};
+
+export async function updateAgencyStatus(req, res) {
+    
+    const agency = await Agency.findById(req.params.agencyId);
+
+    if (!agency) throw createHttpError(404, 'Agency not found');
+
+    Object.assign(agency, { active: !agency.active });
+
+    await agency.save();
+
+    res.json(agency);
+};
---
+++ api/src/api/controllers/locations.controller.js
@@ -0,0 +1,45 @@
+
+import createHttpError from "http-errors";
+import Location from "../../lib/models/location.model";
+
+export const create = async (req, res) => {
+
+    const { 
+        country_code, 
+        country_name, 
+        admin_code, 
+        admin_full_code, 
+        name, 
+        normalized_name, 
+        type } = req.body;
+
+    const location = await Location.create({
+        country_code, 
+        country_name, 
+        admin_code, 
+        admin_full_code, 
+        name, 
+        normalized_name, 
+        type
+    });
+
+    res.status(201).json(location);
+};
+
+export const list = async (req, res) => {
+
+    const locations = Location.find();
+
+    if(!locations) throw createHttpError(404, 'Locations not found');
+
+    res.json(locations);
+};
+
+export const details = async (req, res) => {
+
+    const locations = Location.findById(req.params.locationId);
+
+    if (!locations) throw createHttpError(404, 'Location not found');
+
+    res.json(locations);
+};

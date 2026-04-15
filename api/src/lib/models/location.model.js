6efb9ecb12a68c18e34c03da9a498e19147f529a
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-15 10:49:24 +0200

    feat: add validation rules to locations schema

--- api/src/lib/models/location.model.js
+++ api/src/lib/models/location.model.js
@@ -4,35 +4,46 @@ import mongoose from "mongoose";
 const locationSchema = new mongoose.Schema({
     country_code: {
         type: String,
-        required: true,
-        maxlength: 2,
+        required: [true, 'Código de país obligatorio.'],
+        maxlength: [2, 'Longitud mínima de 2.'],
         uppercase: true
     },
     country_name: {
         type: String,
-        required: true
+        required: [true, 'Nombre de país obligatorio.'],
+        maxlength: [20, 'Longitud mínima de 20.'],
+        set: values => normalizeString(values)
     },
     admin_code: {
         type: String,
-        required: true,
+        required: [true, 'Código provincia obligatorio.'],
         uppercase: true
     },
     admin_full_code: {
         type: String,
-        required: true,
-        uppercase: true,
-        index: true,
-        unique: true
+        required: [true, 'Código admin provincia obligatorio.'],
+        uppercase: true        
     },
     name: {
         type: String,
-        required: true
+        required: [true, 'Nombre de provincia obligatorio.'],
+        maxlength: [20, 'Longitud mínima de 20.'],
+        set: values => normalizeString(values)
     },
     normalized_name: {
         type: String,
-        required: true,
+        required: [true, 'Nombre de provincia normalizado obligatorio.'],
         lowercase: true,
-        index: true
+        index: true,
+        set: values => {
+            return values
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .trim()
+            .toLowerCase()
+            .split(/\s+/)
+            .join('_')
+        }
     },
     type: {
         type: String,
@@ -50,10 +61,26 @@ const locationSchema = new mongoose.Schema({
     }
 });
 
+
 locationSchema.index({ 
     country_code: 1, 
     normalized_name: 1 
 });
+
+locationSchema.index({ 
+    admin_full_code: 1 
+}, { unique: true });
+
+function normalizeString(values) {
+    return values
+        .trim()
+        .toLowerCase()
+        .split(' ')
+        .map(value => 
+            value.charAt(0).toUpperCase() + 
+            value.slice(1))
+        .join(' '); 
+}
 
 const Location = mongoose.model('Location', locationSchema);
 

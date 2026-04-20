90e1647112a9a24eee74ef5e25590d229ea8db1a
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-20 15:58:34 +0200

    feat: add utils api

---
+++ api/src/api/utils/date.util.js
@@ -0,0 +1,11 @@
+
+export const dateFormat = () => {
+
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}

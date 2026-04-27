f590887f37856daff52f2da7115a164a501a6b92
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-27 16:03:59 +0200

    feat: new field seed agency

--- api/src/lib/storages/agency.storage.js
+++ api/src/lib/storages/agency.storage.js
@@ -31,6 +31,15 @@ export const agencies = [
             hasAndaluciaRule: false,
             supportsPallets: true,
             supportsParcels: false
+        },
+        apiConfig: {
+            timeout: 3000,
+            baseUrlApi: 'https://api-gateway.dachser.com/rest/v2',
+            endpoints: {
+                quotations: '/quotations',
+                transportOrders: ''
+            },
+            apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiaS5iMmIucG9ydGFsIiwiZXhwIjoxNzc3MjgzNjE2LCJjdXN0b21lck51bWJlciI6NDYzMzM0NjEsImN1c3RvbWVyTmFtZSI6IlBydWViYSJ9.dak3h0b4urZxva4rDQ12YoIlq7efiPIddGsJaKWaW7k'
         }
     }
 ];

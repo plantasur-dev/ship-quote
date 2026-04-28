324435c4e74dbf1f7c2fd5e540b57a58c9297587
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-28 10:35:18 +0200

    feat: add pages frontend

---
+++ web/src/pages/dashboard-pages.jsx
@@ -0,0 +1,12 @@
+
+import { LayoutPage } from "../components/layouts";
+import { CompareRates } from "../components/ship-quote";
+
+export default function Dashboard() {
+
+    return (
+        <LayoutPage>
+            <CompareRates />
+        </LayoutPage>
+    );
+}

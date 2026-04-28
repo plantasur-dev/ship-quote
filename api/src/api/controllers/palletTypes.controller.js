a3539bea2fa0f4f345e506e00b1a8aae891646b3
rafacrplantasur <r.carrasco@plantasur.com>
2026-04-28 14:04:57 +0200

    feat: new services compare Pallets dimensions

--- api/src/api/controllers/palletTypes.controller.js
+++ api/src/api/controllers/palletTypes.controller.js
@@ -3,6 +3,8 @@ import createHttpError from "http-errors";
 
 import PalletType from "../../lib/models/palletType.model.js";
 
+import { compareDimensions } from '../services/comparePallets.services.js';
+
 export const create = async (req, res) => {
 
     const { agencyId, name, constraints } = req.body;
@@ -35,4 +37,33 @@ export const remove = async (req, res) => {
     await PalletType.findByIdAndDelete(req.params.palletTypeId);
 
     res.status(204).end();
+};
+
+export const compare = async (req, res) => {
+ 
+    const { item } = req.body;
+
+    const maxDimensionns = await PalletType.aggregate([
+        {
+            $group: {
+                _id: null, 
+                maxWidth: { $max: "$constraints.maxWidth" },
+                maxWeight: { $max: "$constraints.maxWeight" },
+                maxLarge: { $max: "$constraints.maxLength"},
+                maxHeight: { $max: "$constraints.maxHeight"}
+            }
+        }
+    ]);
+
+    if (!maxDimensionns) {
+        throw createHttpError(404, 'Calcule dimensions errors');
+    }
+
+    const dimensionsPallet = compareDimensions(item, maxDimensionns);
+
+    if (!dimensionsPallet) {
+        throw createHttpError(404, 'Compare dimensions not founds');
+    }
+
+    res.send();
 };

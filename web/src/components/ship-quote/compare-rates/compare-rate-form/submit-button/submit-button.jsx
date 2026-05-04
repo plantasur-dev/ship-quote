8707992410f1102bc50c7a8ba0cb1d3910286dc8
rafacrplantasur <r.carrasco@plantasur.com>
2026-05-04 16:01:10 +0200

    refactor: add spinner

--- web/src/components/ship-quote/compare-rates/compare-rate-form/submit-button/submit-button.jsx
+++ web/src/components/ship-quote/compare-rates/compare-rate-form/submit-button/submit-button.jsx
@@ -1,30 +1,34 @@
 
 import { useFormContext, useWatch } from "react-hook-form";
 
+import { BounceLoader } from "react-spinners";
+
 function SubmitButton() {
 
-    const { control, formState: { isSubmitting } } = useFormContext();
+    const { 
+        control, 
+        formState: { isSubmitting } 
+    } = useFormContext();
 
-    const items = useWatch({ control, name: "items" });
+    const items = useWatch({ control, name: 'items' });
 
     return (
-        <button 
-            type="submit"
-            disabled={ items.length === 0 }
-            className={`w-full 
-                py-3 
-                rounded-xl 
-                bg-indigo-600 
-                text-white 
-                font-semibold 
-                hover:bg-indigo-700 
-                transition 
-                cursor-pointer
-            `}
-        >
-            { isSubmitting ? "Consultando cotización..." : "Obtener cotización" }
-        </button>
-    );
+    <button 
+        type='submit'
+        disabled={ items.length === 0 || isSubmitting }
+        className='w-full py-3 rounded-xl bg-indigo-600 text-white font-semibold transition cursor-pointer
+                   hover:bg-indigo-700
+                   disabled:opacity-50
+                   disabled:hover:bg-indigo-600'
+    >
+        { isSubmitting 
+            ?   <div className='flex items-center justify-center h-5'>
+                    <BounceLoader color='#ffffff' size={18} />
+                    <span className="ml-1">Consultando cotización...</span>
+                </div>
+            :   'Obtener cotización' }
+    </button>
+);
 }
 
 export default SubmitButton;

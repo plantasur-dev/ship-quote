51665f5babf10816d382f4282ea5dddb6a56794a
rafacrplantasur <r.carrasco@plantasur.com>
2026-05-05 08:23:37 +0200

    feat: add button reset form

--- web/src/components/ship-quote/compare-rates/compare-rate-form/index.js
+++ web/src/components/ship-quote/compare-rates/compare-rate-form/index.js
@@ -1,7 +1,8 @@
 
-export { default as CountrySelector } from "./country-selector/country-selector";
-export { default as PostalCodeInput } from "./postal-code-input/postal-code-input";
-export { default as ProvinceDisplay } from "./province-display/province-display";
-export { default as ItemDraftForm } from "./item-draft-form/item-draft-form";
-export { default as SubmitButton } from "./submit-button/submit-button";
-export { default as ErrorsForm } from "./errors-form/errors-form";
+export { default as CountrySelector } from './country-selector/country-selector';
+export { default as PostalCodeInput } from './postal-code-input/postal-code-input';
+export { default as ProvinceDisplay } from './province-display/province-display';
+export { default as ItemDraftForm } from './item-draft-form/item-draft-form';
+export { default as SubmitButton } from './submit-button/submit-button';
+export { default as ResetButton } from './reset-button/reset-button';
+export { default as ErrorsForm } from './errors-form/errors-form';

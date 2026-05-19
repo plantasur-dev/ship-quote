e79339faa7f426bc76ca1d58ec6603ba83b95497
Rafacruiz <rafacruiz@gmail.com>
2026-05-19 20:55:55 +0200

    style: improve main background gradient and remove text color override

--- web/src/components/layouts/layout-page/layout-page.jsx
+++ web/src/components/layouts/layout-page/layout-page.jsx
@@ -5,7 +5,15 @@ function LayoutPage ({ children }) {
     
     return (
         <>
-            <main className="min-h-screen bg-gradient-to-br from-slate-50 to-indigo-50 text-slate-800">
+            <main
+                className="
+                    min-h-screen
+                    bg-gradient-to-br
+                    from-slate-50
+                    via-slate-50
+                    to-indigo-50/30
+                "
+            >
                 <Navbar />
 
                 { children }

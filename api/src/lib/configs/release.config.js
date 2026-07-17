07cfdbc6e060a9cb05e67cbef9d1a958b9f6c238
rafacrplantasur <r.carrasco@plantasur.com>
2026-07-17 13:19:05 +0200

    fix: update latest release information to version 3.1.1 with new TECUM rates for Portugal

--- api/src/lib/configs/release.config.js
+++ api/src/lib/configs/release.config.js
@@ -1,8 +1,8 @@
 
 export const latestRelease = {
     target: 'api',
-    version: "3.1.0",
-    title: "¡Nuevas tarifas y arreglos!",
-    message: "Se han añadido las tarifas de MRW para Portugal. Se ha corregido el cálculo de algunos bultos para Dachser.",
-    date: "2026-07-16"
+    version: "3.1.1",
+    title: "¡Nuevas tarifas Portugal!",
+    message: "Se han añadido las tarifas de TECUM para Portugal.",
+    date: "2026-07-17"
 };

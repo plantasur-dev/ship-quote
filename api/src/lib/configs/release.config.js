50281818e9b227f3d45f9b8cee04eeeb4b892c4d
rafacrplantasur <r.carrasco@plantasur.com>
2026-07-17 14:01:43 +0200

    fix(release): update title for TECUM Portugal release

--- api/src/lib/configs/release.config.js
+++ api/src/lib/configs/release.config.js
@@ -2,7 +2,7 @@
 export const latestRelease = {
     target: 'api',
     version: "3.1.1",
-    title: "¡Nuevas tarifas Portugal!",
+    title: "¡TECUM Portugal!",
     message: "Se han añadido las tarifas de TECUM para Portugal.",
     date: "2026-07-17"
 };

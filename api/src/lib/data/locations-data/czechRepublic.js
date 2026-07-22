d5d22305ff05a15f2284a97dbfebc761151bb20e
Rafacruiz <rafacruiz@gmail.com>
2026-07-22 22:24:20 +0200

    feat: Add location data for Germany, Italy, Portugal, and Spain
    - Created new data files for Germany, Italy, Portugal, and Spain containing location information including country codes, names, administrative codes, and postal codes.
    - Each file exports an array of location objects for easy access in the application.

---
+++ api/src/lib/data/locations-data/czechRepublic.js
@@ -0,0 +1,63 @@
+
+export const czech = [
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "10" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "11" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "12" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "13" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "14" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "15" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "16" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "17" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "18" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Praha", adminCode: "PR", postalCode: "19" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Středočeský", adminCode: "ST", postalCode: "25" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Středočeský", adminCode: "ST", postalCode: "26" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Středočeský", adminCode: "ST", postalCode: "27" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Středočeský", adminCode: "ST", postalCode: "28" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Středočeský", adminCode: "ST", postalCode: "29" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihočeský", adminCode: "JC", postalCode: "37" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihočeský", adminCode: "JC", postalCode: "38" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihočeský", adminCode: "JC", postalCode: "39" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Plzeňský", adminCode: "PL", postalCode: "30" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Plzeňský", adminCode: "PL", postalCode: "31" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Plzeňský", adminCode: "PL", postalCode: "32" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Karlovarský", adminCode: "KA", postalCode: "35" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Karlovarský", adminCode: "KA", postalCode: "36" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Ústecký", adminCode: "US", postalCode: "40" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Ústecký", adminCode: "US", postalCode: "41" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Ústecký", adminCode: "US", postalCode: "42" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Liberecký", adminCode: "LI", postalCode: "46" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Liberecký", adminCode: "LI", postalCode: "47" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Královéhradecký", adminCode: "KR", postalCode: "50" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Královéhradecký", adminCode: "KR", postalCode: "51" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Pardubický", adminCode: "PA", postalCode: "53" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Pardubický", adminCode: "PA", postalCode: "56" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Vysočina", adminCode: "VY", postalCode: "58" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Vysočina", adminCode: "VY", postalCode: "59" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihomoravský", adminCode: "JM", postalCode: "60" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihomoravský", adminCode: "JM", postalCode: "61" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihomoravský", adminCode: "JM", postalCode: "62" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihomoravský", adminCode: "JM", postalCode: "66" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Zlínský", adminCode: "ZL", postalCode: "68" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Zlínský", adminCode: "ZL", postalCode: "69" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Olomoucký", adminCode: "OL", postalCode: "77" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Olomoucký", adminCode: "OL", postalCode: "78" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Moravskoslezský", adminCode: "MO", postalCode: "70" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Moravskoslezský", adminCode: "MO", postalCode: "71" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Moravskoslezský", adminCode: "MO", postalCode: "72" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Moravskoslezský", adminCode: "MO", postalCode: "73" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Moravskoslezský", adminCode: "MO", postalCode: "74" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Olomoucký", adminCode: "OL", postalCode: "75" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Olomoucký", adminCode: "OL", postalCode: "76" },
+
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihočeský", adminCode: "JC", postalCode: "33" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Jihočeský", adminCode: "JC", postalCode: "34" },
+    { countryCode: "CZ", countryName: "Czech Republic", name: "Vysočina", adminCode: "VY", postalCode: "39" }
+];

5d7f7caa67823f1d8c227eea82a65dfa89b147e6
rafacrplantasur <r.carrasco@plantasur.com>
2026-08-21 10:25:02 +0200

    feat(front:utils): add date utility functions for live clock and formatting

---
+++ web/src/utils/index.js
@@ -0,0 +1,5 @@
+
+export { useLiveClock as useLiveClock } from './date-utils';
+export { formatDay as formatDay } from './date-utils';
+export { formatTime as formatTime } from './date-utils';
+export { formatClock as formatClock } from './date-utils';

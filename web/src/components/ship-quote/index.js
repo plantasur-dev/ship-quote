486255310e7fe3179d129763d38287e26d79d7c1
rafacrplantasur <r.carrasco@plantasur.com>
2026-08-24 08:17:00 +0200

    feat(front:admin-panel): create admin panel exports for dashboard sections

--- web/src/components/ship-quote/index.js
+++ web/src/components/ship-quote/index.js
@@ -1,5 +1,2 @@
 
 export { default as CompareRates } from './compare-rates/compare-rates';
-export { default as Stats } from './admin/dashboard-sections/stats';
-export { default as StateAgencies } from './admin/dashboard-sections/state-agencies';
-export { default as Activity } from './admin/dashboard-sections/activity';

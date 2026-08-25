3de2fbad2325c74ea931d391c4d571397404fa51
rafacrplantasur <r.carrasco@plantasur.com>
2026-08-25 14:55:14 +0200

    feat(front:admin-panel): update index.js to include additional UI components

--- web/src/components/ui/index.js
+++ web/src/components/ui/index.js
@@ -3,3 +3,8 @@ export { default as Navbar } from './navbar/navbar-ui';
 export { default as Footer } from './footer/footer-ui';
 export { default as Alert } from './alert/alert-ui';
 export { default as AlertRelease } from './alert/alert-release-ui';
+export { default as RouteBackdrop } from './route-back-drop/route-back-drop';
+export { default as LoadingScreen } from './loaders/loader';
+export { default as Jumbotron } from './jumbotron/jumbotron';
+export { default as Siderbar } from './siderbar/siderbar';
+export { default as RouteSpinner } from './loaders/loader';

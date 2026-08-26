11259171ce59f949eeeba6fec74aeca09dc0fe37
rafacrplantasur <r.carrasco@plantasur.com>
2026-08-26 09:17:02 +0200

    feat(api): enhance auditsList to support filtering by criteria and pagination

--- api/src/api/controllers/audits.controller.js
+++ api/src/api/controllers/audits.controller.js
@@ -5,14 +5,25 @@ import { auditsList } from "../services/audit.service.js";
 
 export const list = async (req, res) => {
 
+    const criteria = {};
+
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 14;
-    
-    const audits = await auditsList({ page, limit });
+
+    if (req.query.startDate) {
+        const startDate = new Date(req.query.startDate);
+        startDate.setHours(0, 0, 0, 0);
+
+        criteria.createdAt = { $gte: startDate };
+    }
+
+    criteria.endpoint = { $not: /audits/ }
+
+    const audits = await auditsList({ page, limit, criteria });
 
     if (!audits.data.length) {
         throw createHttpError(404, 'Audits not founds');
     }
 
-    return res.json(audits); 
+    return res.json(audits);    
 };
--- api/src/api/services/audit.service.js
+++ api/src/api/services/audit.service.js
@@ -4,15 +4,24 @@ import Audit from "../../lib/models/audit.model.js";
 export const auditStore = async (auditResponse) => 
     await Audit.create(auditResponse);
 
-export const auditsList = async ({ page = 1, limit = 10 }) => {
+export const auditsList = async (params = {}) => {
+    
+    const { page = 1, limit = 10, criteria } = params;
 
     const startIndex = (page - 1) * limit;
     
     const [audits, total] = await Promise.all([
-        Audit.find()
-            .limit(limit)
-            .skip(startIndex),
-        Audit.countDocuments()
+        Audit.find({ ...criteria })
+        .sort({ createdAt: -1 })
+        .populate({
+            path: 'userId',
+            select: 'email username'
+        })
+        
+        .limit(limit)
+        .skip(startIndex),
+
+        Audit.countDocuments({ ...criteria })
     ]);
     
     if (audits.length === 0) return { data: audits };

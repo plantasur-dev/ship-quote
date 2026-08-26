acf3ebb7b9ecbbca92ce9b44ec968d34c2f646b1
rafacrplantasur <r.carrasco@plantasur.com>
2026-08-26 09:16:31 +0200

    feat(api): configure CORS middleware with dynamic origin and credentials

--- api/app.js
+++ api/app.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(httpLogger);
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
 
 app.use(express.json());
 

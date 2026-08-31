eea2190033ec163798a1e693a7a855207b714355
Rafacruiz <rafacruiz@gmail.com>
2026-08-31 20:04:52 +0200

    feat(utils): add wait utility function and update constants exports

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -6,9 +6,12 @@ export { formatClock } from './date-utils';
 
 export { toNumber } from './to-number-utils';
 
+export { wait } from './wait-utils';
+
 export { NAV_ITEMS } from './navigation';
 
 export { STATUS_MESSAGES } from './constants';
+export { REDIRECT_DELAY } from './constants';
 export { TIMER_ACTIVITY  } from './constants';
 export { TYPE_FUEL_SURCHARGE } from './constants';
 export { TYPE_AGENCY } from './constants';

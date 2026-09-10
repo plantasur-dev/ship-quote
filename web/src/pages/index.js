a8bbff96ff8dc7361d283252bf1e63b99666e350
rafacrplantasur <r.carrasco@plantasur.com>
2026-09-10 15:50:58 +0200

    refactor(front): remove unused admin panel page (import lazy) exports from index

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,3 @@
 
 export { default as HomePage } from './home-page';
-
-export { default as Dashboard } from './admin/dashboard/dashboard-page';
-export { default as LoginPage } from './admin/auth/login-page';
-export { default as OverviewAgenciesPage } from './admin/agencies/overview-agencies-page';
-export { default as CreateAgencyPage } from './admin/agencies/create-agency-page';
-export { default as AgencyPage } from './admin/agencies/agency-page';
-export { default as PanelAuditsPage } from './admin/audits/panel-audits-page';
-export { default as AuditPage} from './admin/audits/audit-page';
-
 export { default as NotFoundPage } from './404-page';

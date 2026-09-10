8e716dffdd1cbdb3203290d705eb61d401069eed
rafacrplantasur <r.carrasco@plantasur.com>
2026-09-10 10:11:52 +0200

    feat(front:ui): add ErrorState component for improved error handling

--- web/src/components/ui/index.js
+++ web/src/components/ui/index.js
@@ -3,6 +3,7 @@ export { default as Navbar } from './navbar/navbar-ui';
 export { default as Footer } from './footer/footer-ui';
 export { default as Alert } from './alert/alert-ui';
 export { default as AlertRelease } from './alert/alert-release-ui';
+export { default as ErrorState } from './errors/error-state';
 export { default as RouteBackdrop } from './route-back-drop/route-back-drop';
 export { default as LoadingScreen } from './loaders/loader';
 export { default as Jumbotron } from './jumbotron/jumbotron';
